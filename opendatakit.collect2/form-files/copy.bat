cce8b442f9e3a5ca3375dd9b99da82c623cb67c7
Nathan Breit <nabreit@gmail.com>
2012-12-10 18:01:09 -0500

    completely restructuring for use with cordova.

--- opendatakit.collect2/form-files/copy.bat
+++
@@ -1,9 +0,0 @@
-rmdir /s /q C:\Program Files\Apache Software Foundation\Apache2.4\htdocs\FORM\collect"
-rmdir /s /q C:\Program Files\Apache Software Foundation\Apache2.4\htdocs\FORM\json-media"
-rmdir /s /q C:\Program Files\Apache Software Foundation\Apache2.4\htdocs\FORM\lgform"
-rmdir /s /q C:\Program Files\Apache Software Foundation\Apache2.4\htdocs\FORM\example"
-xcopy . "C:\Program Files\Apache Software Foundation\Apache2.4\htdocs\FORM" /E /I /R /Y
-del "C:\Program Files\Apache Software Foundation\Apache2.4\htdocs\FORM\copy.bat"
-del "C:\Program Files\Apache Software Foundation\Apache2.4\htdocs\FORM\copyMnt.bat"
-echo # %DATE% %TIME% >> "C:\Program Files\Apache Software Foundation\Apache2.4\htdocs\FORM\collect\manifest.appcache"
-# pause 'Hit enter to continue'

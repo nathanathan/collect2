ec42010ebc49c28f026d459c26959ffceb630ea2
Nathan Breit <nabreit@gmail.com>
2012-12-10 19:47:42 -0500

    Adding dirlist code

--- apis/file.js
+++ apis/file.js
@@ -53,6 +53,32 @@ function createFile() { // button onclick function
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, failFS);
     }
 }
+function getDirList() {
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+        console.log(fileSystem.name);
+        console.log(fileSystem.root.name);
+        $('#file-system-text').html("File System: <strong>" + fileSystem.name + "</strong> " +
+                "Root: <strong>" + fileSystem.root.name + "</strong>");
+        
+        fileSystem.root.getDirectory("newDir", {create: true, exclusive: false}, function(dirEntry){
+            var directoryReader = dirEntry.createReader();
+            // Get a list of all the entries in the directory
+            directoryReader.readEntries(function(entries){
+                var i;
+                var $entries = $('#entries');
+                for (i=0; i<entries.length; i++) {
+                    console.log(entries[i].name);
+                    $entries.append('<p>' + entries[i].name + '</p>');
+                }
+            }, function(error){
+                alert("Failed to list directory contents: " + error.code);
+            });
+        },
+        function(error){
+            alert("Unable to create new directory: " + error.code);
+        });
+    }, failFS);
+}
 
 //api-file  FileWriter
 function gotFileWriter(writer) {

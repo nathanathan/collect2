8cf0f06811bca2ddc537e40bd54987a8ddfa9a15
Nathan Breit <nabreit@gmail.com>
2012-12-19 21:03:29 -0500

    Adding sorting

--- FLP/router.js
+++ FLP/router.js
@@ -4,9 +4,7 @@ define([
 	'underscore',
     'text!../dirListView.html'], 
 function($, Backbone, _, dirListView){
-    var compiledTemplate = _.template(dirListView);
-    var $container =  $('#container');
-
+    
     function getDirList(dirPath, callback) {
         if(!('requestFileSystem' in window)) {
             alert('Cannot call requestFileSystem');
@@ -48,17 +46,7 @@ function($, Backbone, _, dirListView){
                 var directoryReader = dirEntry.createReader();
                 // Get a list of all the entries in the directory
                 directoryReader.readEntries(function(entries) {
-                    var afterMetadataAttached = _.after(entries.length, function() {
-                        callback(entries);
-                    });
-                    _.each(entries, function(entry){
-                        entry.getMetadata(function success(metaData){
-                            entry._modificationTime = metaData.modificationTime;
-                            afterMetadataAttached();
-                        }, function fail(err){
-                            alert("Could not get metadata");
-                        });
-                    });
+                    callback(entries);
                 }, function(error) {
                     alert("Failed to list directory contents: " + error.code);
                 });
@@ -70,6 +58,79 @@ function($, Backbone, _, dirListView){
             alert("File System Error: " + evt.target.error.code);
         });
     }
+    
+    var DirView = Backbone.View.extend({
+        template: _.template(dirListView),
+        orderVar: 1,
+        render: function() {
+            console.log('render');
+            this.$el.html(this.template({
+                entries : this.collection.toJSON()
+            }));
+            return this;
+        },
+        events: {
+            'click .sort-name' : 'sortName',
+            'click .sort-date' : 'sortDate',
+            'click .refresh' : 'refresh'
+        },
+        genComparator : function(cfunc, incr) {
+            if(!incr) {
+                incr = 1;
+            }
+            return function(Ain, Bin) {
+                var A = cfunc(Ain);
+                var B = cfunc(Bin);
+                if(A < B) return -incr;
+                if(A > B) return incr;
+                if(A == B) return 0;
+            };
+        },
+        sortName: function(e){
+            console.log('sortName');
+            console.log(e);
+            this.orderVar = -this.orderVar;
+            this.collection.comparator = this.genComparator(function(entry){
+                return entry.get("_modificationTime");
+            }, this.orderVar);
+            this.collection.sort();
+            this.render();
+            return this;
+        },
+        sortDate: function(e){
+            console.log('sortTime');
+            console.log(e);
+            this.orderVar = -this.orderVar;
+            this.collection.comparator = this.genComparator(function(entry){
+                return entry.get("name");
+            }, this.orderVar);
+            this.collection.sort();
+            this.render();
+            return this;
+        },
+        refresh: function(e){
+            console.log('refresh');
+            console.log(e);
+            var that = this;
+            getDirList(that.dirPath, function(entries){
+                var afterMetadataAttached = _.after(entries.length, function() {
+                    that.collection.reset(entries);
+                    that.render();
+                });
+                //Go through all the entries and asynchronously get their metadatas.
+                _.each(entries, function(entry){
+                    entry.getMetadata(function success(metaData){
+                        entry._modificationTime = metaData.modificationTime;
+                        console.log(entry._modificationTime);
+                        afterMetadataAttached();
+                    }, function fail(err){
+                        alert("Could not get metadata");
+                    });
+                });
+            });
+            return this;
+        }
+    });
     
     
     //indexRelPathPrefix computed so the location of the boilerplate directory can change
@@ -95,7 +156,6 @@ function($, Backbone, _, dirListView){
             this.navigate('listFiles', {trigger: true, replace: true});
         },
 		listFiles: function(page, params){
-            var that = this;
             console.log('params:');
             console.log(params);
             if(!params){
@@ -104,22 +164,13 @@ function($, Backbone, _, dirListView){
             _.extend(params, {
                 "dirPath" : "odk/js/forms/"
             });
-            getDirList(params.dirPath, function(entries) {
-                console.log(entries);
-                $container.html(compiledTemplate({"entries" : entries}));
-                /*
-                var i;
-                var $entries = $('#entries');
-                for (i = 0; i < entries.length; i++) {
-                    console.log(entries[i].name);
-                    if (entries[i].isDirectory) {
-                        var $formLink = $('<li><a href="file:///sdcard/' + dirPath + entries[i].name + '/formDef.json">' + entries[i].name + '</a></li>');
-                        $entries.append($formLink);
-                    }
-                }
-                */
-            });
             
+            var myDirView = new DirView({
+                collection: new Backbone.Collection(),
+                dirPath: params.dirPath
+            });
+            $('#container').append(myDirView.render().el);
+            myDirView.refresh();
 		}
 	});
 	return Router;
